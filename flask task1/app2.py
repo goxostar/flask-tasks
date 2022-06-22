189584129f45c4ac96646da22e4de676a02f8afd
goxostar <goktugekinci44@gmail.com>
2022-06-22 17:15:40 +0300

    Routing & Templates

--- flask task1/app2.py
+++ flask task1/app2.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, g
+from flask import Flask, g, redirect, render_template, flash, url_for
 from flask_oidc import OpenIDConnect
 
 app = Flask(__name__)
@@ -17,31 +17,32 @@ app.config.update({
 oidc = OpenIDConnect(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def hello_world():
     if oidc.user_loggedin:
         return ('Hi, %s, <a href="/dashboard">See dashboard</a> '
                 '<a href="/logout">Log out</a>') % \
             oidc.user_getfield('email')
     else:
-        return 'Welcome Newcomer, <a href="/dashboard">Log in</a>'
+        return render_template('home.html')
+        #return 'Welcome Newcomer, <a href="/dashboard">Log in</a>'
 
 
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET','POST'])
 @oidc.require_login
-def hello_me():
+def dashboard():
     info = oidc.user_getinfo(['email', 'openid_id'])
     return ('Hello, your email is %s ! <a href="/">Go Back</a>' %
             (info.get('email')))
 
 
-@app.route('/api')
+@app.route('/api', methods=['GET','POST'])
 @oidc.accept_token(True, ['openid'])
 def hello_api():
     return json.dumps({'hello': 'Welcome %s' % g.oidc_token_info['sub']})
 
 
-@app.route('/logout')
+@app.route('/logout', methods=['GET','POST'])
 def logout():
     oidc.logout()
     return 'You have been logged out! <a href="/">Home</a>'
