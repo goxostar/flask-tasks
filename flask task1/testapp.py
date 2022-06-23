482caff6d39d104ad3d3cdd04ed8faa29288a190
goxostar <goktugekinci44@gmail.com>
2022-06-23 14:27:37 +0300

    request to keycloak api

--- flask task1/testapp.py
+++ flask task1/testapp.py
@@ -1,4 +1,5 @@
 import json
+import requests
 from flask import Flask, g, redirect, render_template, flash, url_for, session
 from flask_oidc import OpenIDConnect
 from keycloak import KeycloakOpenID, KeycloakAdmin
@@ -12,6 +13,19 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
+url = "http://localhost:8080/auth/realms/demo-realm/protocol/openid-connect/token"
+ 
+header = {    
+    "Content-Type":"application/x-www-form-urlencoded"    
+    }
+ 
+rr ="grant_type=client_credentials&client_id=client1&client_secret=f720daa4-059b-4ba6-b34d-e898fab1e6ae"
+response = requests.post(url, data=rr, headers=header, verify=False)
+ 
+print("Status Code", response.status_code)
+print("JSON Response ", response.json())
 
 class LoginForm(FlaskForm):
   username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
